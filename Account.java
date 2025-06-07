aca127e1bbeb2a3885b8851d1153803e65345c63
Arnav-304 <kumararnav304@gmail.com>
2025-06-07 17:22:43 +0530

    Create Account.java

---
+++ Account.java
@@ -0,0 +1,13 @@
+public class Account {
+    private int accountId;
+    private String holderName;
+    private double balance;
+
+    public Account(int accountId, String holderName, double balance) {
+        this.accountId = accountId;
+        this.holderName = holderName;
+        this.balance = balance;
+    }
+
+    // Getters and setters
+}
